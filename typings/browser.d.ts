6f3b7442206a96eabd0f875cf20994783b2936f0
David Coxon <David Coxon>
2016-04-27 00:17:31 +0100

    Added git ignore to remove typings folder from source control.

--- typings/browser.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/promises-a-plus/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/promises-a-plus/index.d.ts" />
